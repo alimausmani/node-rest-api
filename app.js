c668dd22206cb08a210d5f7bf28af926066cb6aa
Alima <alimausmani21@navgurukul>
2024-09-04 23:49:46 +0530

    Apply cors in app.js

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 const express = require('express');
-const app =express();
+const app = express();
+const cors = require('cors');
 
+// Apply CORS middleware
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 const products_routes = require('./routes/products');
-
 const Connect = require('./db/connect');
 
-
-app.get('/',(req,res)=>{
-    res.send("Hi i am Live");
+app.get('/', (req, res) => {
+    res.send("Hi, I am Live");
 });
 
-// middleware or to set router
+// Middleware or to set router
+app.use('/api/products', products_routes);
 
-
-app.use('/api/products',products_routes);
-
-
-
-
-
-
-
-const start = async()=>{
-    try{
-        app.listen(PORT,()=>{
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
             Connect();
             console.log(`${PORT} Connected to the server`);
-
-        })
-    }catch(error){
+        });
+    } catch (error) {
         console.log(error);
-        
     }
-}
+};
 
 start();
